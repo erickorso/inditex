2079072dea5318aa71c48d0b14542958142efede
Erick Vargas <erickorso@gmail.com>
2023-07-09 19:59:06 -0300

    axios fix

--- lib/services/axios.api.ts
+++ lib/services/axios.api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const isLocal = process.env.NODE_ENV === 'development';
-const baseUrl = isLocal ? 'http://localhost:3000' : process.env.NEXT_PUBLIC_VERCEL_URL
+const baseUrl = isLocal ? 'http://localhost:3000' : ''
 const API = axios.create({ baseURL: `${baseUrl}/api/podcasts`});
 
 API.interceptors.request.use((req) => {
