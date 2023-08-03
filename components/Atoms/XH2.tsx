3ce8d29b1541044031287ee8964c0d4fcf29aa1a
Erick Vargas <erickorso@gmail.com>
2023-08-03 06:56:37 -0300

    tailwind & modal

--- components/Atoms/XH2.tsx
+++ components/Atoms/XH2.tsx
@@ -1,16 +1,27 @@
 import { FC, ReactNode } from 'react'
 import styled from 'styled-components'
 
-const H2Styled = styled.h2`
-    font-size: 20px;
-    color: #222;
+interface H2Props {
+    size?: string;
+    color?: string;
+}
+
+const H2Styled = styled.h2<H2Props>`
+    font-size: ${(props) => props.size ? props.size + 'px' : '20px'};
+    color: ${(props) => props.color ? props.color : '#222'};
     font-weight: bolder;
 `
 
 type XH2Type = {
-    title: string | ReactNode
+    title: string | ReactNode,
+    color?: string,
+    size?: string,
 }
 
-const XH2: FC<XH2Type> = ({title}) => <H2Styled>{title}</H2Styled>
+const XH2: FC<XH2Type> = ({ title, color, size }) => (
+    <H2Styled color={color} size={size}>
+        {title}
+    </H2Styled>
+)
 
 export default XH2
